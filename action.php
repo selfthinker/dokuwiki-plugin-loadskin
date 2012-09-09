3a2e5e42b19e03bcc8ec29124bac8bcdbed8b1ac
Anika Henke <anika@selfthinker.org>
2012-09-10 00:05:31 +0100

    fixed chosen plugin to also show in mediamanager and image detail page (closes #4); also deprecated _defineConstants() since new tpl_basedir() makes it obsolete (closes #5)

--- action.php
+++ action.php
@@ -23,7 +23,13 @@ class action_plugin_loadskin extends DokuWiki_Action_Plugin {
     // register hook
     function register(&$controller) {
         $controller->register_hook('DOKUWIKI_STARTED', 'BEFORE', $this, '_handleConf');
-        $controller->register_hook('DOKUWIKI_STARTED', 'AFTER', $this, '_defineConstants');
+        $controller->register_hook('MEDIAMANAGER_STARTED', 'BEFORE', $this, '_handleConf');
+        $controller->register_hook('DETAIL_STARTED', 'BEFORE', $this, '_handleConf');
+        /* since tpl_basedir() and tpl_incdir() exist and are used instead of DOKU_TPL and
+            DOKU_TPLINC, _defineConstants() isn't needed anymore */
+        if (!function_exists('tpl_basedir')) {
+            $controller->register_hook('DOKUWIKI_STARTED', 'AFTER', $this, '_defineConstants');
+        }
         $controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE', $this, '_handleMeta');
         $controller->register_hook('TPL_CONTENT_DISPLAY', 'BEFORE', $this, '_handleContent', array());
     }
@@ -31,6 +37,7 @@ class action_plugin_loadskin extends DokuWiki_Action_Plugin {
     /**
      * Define DOKU_TPL and DOKU_TPLINC after $conf['template'] has been overwritten
      *  (this still needs the original constant definition in init.php to be removed)
+     * @deprecated DOKU_TPL and DOKU_TPLINC are deprecated since Adora Belle
      *
      * @author Anika Henke <anika@selfthinker.org>
      */
