2682d62f496d3b0e0c7625e15b9c2b2574033e8e
Nicolas Friedli <nicolas@theologique.ch>
2015-06-20 18:00:37 +0200

    translation update

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -4,9 +4,11 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
+ * @author Nicolas Friedli <nicolas@theologique.ch>
  */
 $lang['automaticOutput']       = 'Afficher automatiquement le sélecteur de modèle sur chaque page ?';
 $lang['excludeTemplates']      = 'Exclure ces modèles du sélecteur de modèle (liste à virgule)';
 $lang['mobileSwitch']          = 'Afficher le sélecteur de modèle pour mobile (avec seulement 1 modèle normal et 1 modèle pour mobile) ?';
 $lang['mobileTemplate']        = 'modèle pour mobile (si option afficher sélecteur de modèle pour mobile est activée)';
 $lang['preferUserChoice']      = 'Laisser l\'utilisateur outrepasser les choix de l\'administrateur ?';
+$lang['inheritInTranslations'] = 'Hériter du choix du modèle pour un espace de nom didié à une langue (lorsque l\'extension de traduction est utilisée)?';
