f74af3fdc6c0f49c30c03cff1188680fd470f44c
Hideaki SAWADA <chuno@live.jp>
2015-07-12 04:50:06 +0200

    translation update

---
+++ lang/ja/lang.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['template']              = 'テンプレート';
+$lang['pattern']               = '名前空間・ページのパターン';
+$lang['action']                = '操作';
+$lang['menu']                  = 'LoadSkin テンプレート管理';
+$lang['switchTpl']             = 'テンプレート切替';
+$lang['switch']                = '切替';
+$lang['switchFull']            = '通常用';
+$lang['switchMobile']          = 'モバイル用';
---
+++ lang/ja/settings.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['automaticOutput']       = '全ページにテンプレート切替を自動的に表示する？';
+$lang['excludeTemplates']      = 'テンプレート切替から除外するテンプレート（カンマ区切り一覧）';
+$lang['mobileSwitch']          = 'モバイル用テンプレート切替（通常用テンプレートとモバイル用テンプレート）を表示する？';
+$lang['mobileTemplate']        = 'モバイル用テンプレート（モバイル用テンプレート切替を表示する場合）';
+$lang['preferUserChoice']      = '管理者指定テンプレートよりユーザー選択テンプレートを優先する？';
+$lang['inheritInTranslations'] = '言語別名前空間内で名前空間毎のテンプレート選択を継承する（translation plugin を使用する場合）？';
