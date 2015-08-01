3aeaf1c03e4f72c21ee622452c8ec21ea8366f75
hugo smet <hugo.smet@scarlet.be>
2015-08-02 00:30:06 +0200

    translation update

--- lang/nl/settings.php
+++ lang/nl/settings.php
@@ -4,9 +4,11 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Gerrit Uitslag <klapinklapin@gmail.com>
+ * @author hugo smet <hugo.smet@scarlet.be>
  */
 $lang['automaticOutput']       = 'Automatisch de templatewisselaar op elke pagina plaatsen?';
 $lang['excludeTemplates']      = 'Deze templates uitsluiten van de templatewisselaar (kommagescheiden lijst)';
 $lang['mobileSwitch']          = 'Mobiele templatewisselaar weergeven (met alleen 1 normale en 1 mobiele template)?';
 $lang['mobileTemplate']        = 'Mobiele template (als de mobiele templatewisselaar wordt weergegeven)';
 $lang['preferUserChoice']      = 'De template keuze van de gebruiker prefereren voor de keuze van de beheerder?';
+$lang['inheritInTranslations'] = 'Overname van de template keuze per naamruimte binnenin een taal gebonden naamruimte (wanneer de translation plugin gebruikt is)?';
