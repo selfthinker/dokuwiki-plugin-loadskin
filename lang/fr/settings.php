c46b716999a33f79bba1fa16625bf011d624d52d
Morgan <morgan.leguen@gmail.com>
2016-02-19 14:20:20 +0100

    translation update

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -4,9 +4,11 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
+ * @author Morgan <morgan.leguen@gmail.com>
  */
 $lang['automaticOutput']       = 'Afficher automatiquement le sélecteur de modèle sur chaque page ?';
 $lang['excludeTemplates']      = 'Exclure ces modèles du sélecteur de modèle (liste à virgule)';
 $lang['mobileSwitch']          = 'Afficher le sélecteur de modèle pour mobile (avec seulement 1 modèle normal et 1 modèle pour mobile) ?';
 $lang['mobileTemplate']        = 'modèle pour mobile (si option afficher sélecteur de modèle pour mobile est activée)';
 $lang['preferUserChoice']      = 'Laisser l\'utilisateur outrepasser les choix de l\'administrateur ?';
+$lang['inheritInTranslations'] = 'Hériter du choix de catégorie dans une même catégorie de langue (si le plugin de traduction est utilisé) ?';
