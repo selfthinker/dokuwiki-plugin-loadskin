e5ce24bd1dd482b35f9524ccc4d1a5204105b264
Anika Henke <anika@selfthinker.org>
2016-04-06 23:53:14 +0100

    update French language files (closes #14 #15 #16 #20 #23)

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -2,14 +2,14 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
  */
 $lang['template']              = 'Modèle';
-$lang['pattern']               = 'patron de page/catégorie';
+$lang['pattern']               = 'Patron de page/catégorie';
 $lang['action']                = 'Action';
 $lang['menu']                  = 'Gestionnaire du sélecteur de modèle';
-$lang['switchTpl']             = 'changer de modèle';
+$lang['switchTpl']             = 'Changer de modèle';
 $lang['switch']                = 'Changer';
-$lang['switchFull']            = 'Version complète';
-$lang['switchMobile']          = 'version pour mobile';
+$lang['switchFull']            = 'Version plein écran';
+$lang['switchMobile']          = 'Version pour mobile';
--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -2,11 +2,12 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
  */
 $lang['automaticOutput']       = 'Afficher automatiquement le sélecteur de modèle sur chaque page ?';
 $lang['excludeTemplates']      = 'Exclure ces modèles du sélecteur de modèle (liste à virgule)';
 $lang['mobileSwitch']          = 'Afficher le sélecteur de modèle pour mobile (avec seulement 1 modèle normal et 1 modèle pour mobile) ?';
-$lang['mobileTemplate']        = 'modèle pour mobile (si option afficher sélecteur de modèle pour mobile est activée)';
-$lang['preferUserChoice']      = 'Laisser l\'utilisateur outrepasser les choix de l\'administrateur ?';
+$lang['mobileTemplate']        = 'Modèle pour mobile (si l\'option "afficher le sélecteur de modèles pour mobile" est cochée)';
+$lang['preferUserChoice']      = 'Autoriser l\'utilisateur à outrepasser les choix de l\'administrateur ?';
+$lang['inheritInTranslations'] = 'Hériter le choix du modèle par catégorie à l\'intérieur d\'une catégorie de langue (quand le plugin "Translation" est utilisé) ?';
