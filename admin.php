selfthinker/dokuwiki-plugin-loadskin

commit 45e6b4a00ab2b06d968add3a6ef56c1557cb0bf4

use $INPUT everywhere

admin.php
@@ -36,7 +36,7 @@ class admin_plugin_loadskin extends DokuWiki_Admin_Plugin {
         global $INPUT;
         $data = array();
 
-        if(!empty($_REQUEST['pattern'])) {
+        if(!$INPUT->str('pattern')) {
             if (!checkSecurityToken()) return;
             $id = cleanID($INPUT->str('pattern'));
 

action.php
@@ -59,6 +59,7 @@ class action_plugin_loadskin extends DokuWiki_Action_Plugin {
      */
     public function _handleConf(Doku_Event $event, $param) {
         global $conf;
+        global $ACT;
 
         // store original template in helper attribute
         $helper = $this->loadHelper('loadskin', true);
@@ -66,7 +67,7 @@ class action_plugin_loadskin extends DokuWiki_Action_Plugin {
 
         // set template
         $tpl = $this->_getTpl();
-        if($tpl && $_REQUEST['do'] != 'admin') {
+        if($tpl && ($ACT != 'admin')) {
             $conf['template'] = $tpl;
         }
     }
@@ -117,6 +118,8 @@ class action_plugin_loadskin extends DokuWiki_Action_Plugin {
      * @author Anika Henke <anika@selfthinker.org>
      */
     private function _getTplPerUser() {
+        global $INPUT;
+
         // get all available templates
         $helper = $this->loadHelper('loadskin', true);
         $tpls   = $helper->getTemplates();
@@ -124,8 +127,8 @@ class action_plugin_loadskin extends DokuWiki_Action_Plugin {
         $mobileSwitch = $this->getConf('mobileSwitch');
         $user = $_SERVER['REMOTE_USER'];
 
-        $tplRequest = $_REQUEST['tpl'];
-        $actSelect  = $_REQUEST['act'] && ($_REQUEST['act']=='select');
+        $tplRequest = $INPUT->str('tpl');
+        $actSelect  = $INPUT->str('act') && ($INPUT->str('act') == 'select');
 
         // if template switcher was used
         if ($tplRequest && $actSelect && (in_array($tplRequest, $tpls) || ($tplRequest == '*') )) {

commit be948cce7179ddca3b06d1752e377a43a9537c68

Fix Security Token issue breaking admin form (closes #28)
Introduced by 45e6b4a00ab2b06d968add3a6ef56c1557cb0bf4

admin.php
@@ -36,7 +36,7 @@ class admin_plugin_loadskin extends DokuWiki_Admin_Plugin {
         global $INPUT;
         $data = array();
 
-        if(!$INPUT->str('pattern')) {
+        if($INPUT->str('pattern')) {
             if (!checkSecurityToken()) return;
             $id = cleanID($INPUT->str('pattern'));
 