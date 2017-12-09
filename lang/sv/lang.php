a0f824572f3d459bac01ab644e6d253027a8dd65
Tor Härnqvist <tor@harnqvist.se>
2017-12-09 20:50:11 +0100

    translation update

---
+++ lang/sv/lang.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Tor Härnqvist <tor@harnqvist.se>
+ */
+$lang['template']              = 'Templat';
+$lang['pattern']               = 'Namnrymd-/sid-mönster';
+$lang['action']                = 'Händelse';
+$lang['menu']                  = 'LoadSkin-templathanterare';
+$lang['switchTpl']             = 'Växla templat';
+$lang['switch']                = 'Växla';
+$lang['switchFull']            = 'Fullskalig version';
+$lang['switchMobile']          = 'Mobilversion';
